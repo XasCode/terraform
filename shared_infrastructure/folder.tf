ebc079bb0deee3c594e7a6f673c3b63edf6e9361
Justin <JEStaubach@users.noreply.github.com>
2021-03-14 17:51:30 -0400

    Add Backups under Shared Infrastructure folder, w/ Test environment folder.

--- shared_infrastructure/folder.tf
+++ shared_infrastructure/folder.tf
@@ -15,6 +15,16 @@ resource "google_folder" "shared_infrastructure" {
   parent       = "organizations/${var.organization_id}"
 }
 
+  resource "google_folder" "shared-backups" {
+    display_name = "Backups"
+    parent       = "folders/${google_folder.shared_infrastructure.folder_id}"
+  }
+
+    resource "google_folder" "shared-backups-test" {
+      display_name = "Test"
+      parent       = "folders/${google_folder.shared-backups.folder_id}"
+    }
+
 output "shared_infrastructure" {
   value = google_folder.shared_infrastructure.folder_id
 }
