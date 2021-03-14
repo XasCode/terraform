26c1a15fb774b9f05a4e142e33f57cf822d2507b
Justin <JEStaubach@users.noreply.github.com>
2021-03-13 20:46:12 -0500

    Declare input variables.

--- shared_infrastructure/folder.tf
+++ shared_infrastructure/folder.tf
@@ -1,8 +1,20 @@
-output "shared_infrastructure" {
-  value = google_folder.shared_infrastructure.folder_id
+variable "organization_name" {
+  type = string
+}
+
+variable "organization_id" {
+  type = string
+}
+
+variable "billing_account" {
+  type = string
 }
 
 resource "google_folder" "shared_infrastructure" {
   display_name = "shared_infrastructure"
   parent       = "organizations/${var.organization_id}"
+}
+
+output "shared_infrastructure" {
+  value = google_folder.shared_infrastructure.folder_id
 }
