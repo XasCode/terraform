61fc66c3db734f69fdfa5b540c6a7543bd621cda
Justin <JEStaubach@users.noreply.github.com>
2021-06-26 14:49:15 -0400

    inc add role for snapshot checker

--- terraform/vendor/modules/ou/main.tf
+++ terraform/vendor/modules/ou/main.tf
@@ -58,3 +58,111 @@ module "snapshots" {
 
   depends_on = [module.ou]
 }
+
+resource "random_id" "random" {
+  byte_length = 3
+}
+
+resource "google_project_iam_custom_role" "role-svc-check-snapshots" {
+  role_id     = "role-svc-check-snapshots-${random_id.random.hex}"
+  project     = module.security.id
+  title       = "role-svc-check-snapshots-${random_id.random.hex}"
+  description = "Role / permissions to assign to service account for automatically setting up disk snapshots."
+  permissions = [
+    "compute.disks.list",
+    "compute.snapshots.list",
+    "compute.instances.list",
+    "compute.regions.list",
+    "compute.zones.list",
+    "compute.disks.addResourcePolicies",
+    "compute.resourcePolicies.create",
+    "compute.resourcePolicies.get",
+    "compute.resourcePolicies.list",
+    "compute.resourcePolicies.use",
+    "resourcemanager.organizations.get",
+    "resourcemanager.folders.get",
+    "resourcemanager.folders.list",
+    "resourcemanager.projects.get",
+    "resourcemanager.projects.list",
+    "storage.objects.create"
+  ]
+
+  depends_on = [module.security]
+}
+
+#resource "google_service_account" "svc-check-snapshots" {
+#  account_id   = "svc-check-snapshots-${random_id.random.hex}"
+#  display_name = "Service account for automatically setting up disk snapshots."
+#  project      = module.security.id
+#
+#  depends_on = [module.security]
+#}
+#
+#resource "google_folder_iam_binding" "ou_folder" {
+#  folder  = module.ou.name
+#  role    = google_project_iam_custom_role.role-svc-check-snapshots.role_id
+#
+#  members = [
+#    "serviceAccount:${google_service_account.svc-check-snapshots.account_id}@${module.security.name}.iam.gserviceaccount.com",
+#  ]
+#
+#  depends_on = [module.ou]
+#}
+#
+#resource "google_pubsub_topic" "pubsub-snapshots" {
+#  name = "pubsub-${module.snapshots.name}-${random_id.random.hex}"
+#
+#  project = module.snapshots.id
+#
+#  depends_on = [module.snapshots]
+#}
+#
+#resource "google_cloud_scheduler_job" "scheduler-job-snapshots" {
+#  name        = "scheduler-job-${module.snapshots.name}-${random_id.random.hex}"
+#  description = "scheduler-job-${module.snapshots.name}-${random_id.random.hex}"
+#  schedule    = "0 5 * * *"
+#
+#  project = module.snapshots.id
+#
+#  pubsub_target {
+#    topic_name = google_pubsub_topic.pubsub-snapshots.id
+#  }
+#
+#  retry_config {
+#    retry_count = 0
+#    max_retry_duration = "0s"
+#    min_backoff_duration = "5s"
+#    max_backoff_duration = "3600s"
+#    max_doublings = 5
+#  }
+#
+#  depends_on = [module.snapshots]
+#}
+
+#resource "google_cloudfunctions_function" "function-snapshots" {
+#  name        = "function-${module.snapshots.name}-${random_id.random.hex}"
+#  description = "function-${module.snapshots.name}-${random_id.random.hex}"
+#  runtime     = "nodejs14"
+#
+#  available_memory_mb   = 256
+#  #source_archive_bucket = google_storage_bucket.bucket.name
+#  #source_archive_object = google_storage_bucket_object.archive.name
+#  #trigger_http          = true
+#  timeout               = 60
+#  #entry_point           = "helloGET"
+#  #labels = {
+#  #  my-label = "my-label-value"
+#  #}
+#
+#  #environment_variables = {
+#  #  MY_ENV_VAR = "my-env-var-value"
+#  #}
+#
+#  event_trigger {
+#      event_type= "google.pubsub.topic.publish"
+#      #resource= "projects/${module.snapshots.id}/topics/cloud-builds-topic"
+#      resource = google_pubsub_topic.pubsub-snapshots.id
+#      #service= "pubsub.googleapis.com"
+#      #failure_policy= {}
+#   }
+#}
