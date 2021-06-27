718cc7a77da0996a5eda18b3101b241702e6626c
Justin <JEStaubach@users.noreply.github.com>
2021-06-26 23:44:44 -0400

    enable secretmanager

--- terraform/vendor/modules/ou/main.tf
+++ terraform/vendor/modules/ou/main.tf
@@ -238,7 +238,7 @@ resource "google_project_service" "secretmanager" {
 
 resource "google_secret_manager_secret" "secret-basic" {
   secret_id = "SENDGRID_API_KEY"
-  project = module.snapshots
+  project = module.snapshots.id
 
   replication {
     automatic = true
