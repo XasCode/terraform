22705ae80eb52a73c30a45b77a6f74e77611d842
Justin <JEStaubach@users.noreply.github.com>
2021-06-27 21:05:56 -0400

    add secretmanager.viewer permission

--- terraform/vendor/modules/ou/main.tf
+++ terraform/vendor/modules/ou/main.tf
@@ -25,22 +25,22 @@ module "terraform" {
   depends_on = [module.ou]
 }
 
-module "security" {
-  source = "./vendor/modules/project"
-  
-  name   = "security"
-  parent = {
-    name = module.ou.name
-    path = module.ou.path
-  }
-
-  billing_account = var.billing_account
-
-  envs   = var.envs
-  environment    = var.environment
-
-  depends_on = [module.ou]
-}
+#module "security" {
+#  source = "./vendor/modules/project"
+#  
+#  name   = "security"
+#  parent = {
+#    name = module.ou.name
+#    path = module.ou.path
+#  }
+#
+#  billing_account = var.billing_account
+#
+#  envs   = var.envs
+#  environment    = var.environment
+#
+#  depends_on = [module.ou]
+#}
 
 module "snapshots" {
  source = "./vendor/modules/project"
@@ -60,7 +60,7 @@ module "snapshots" {
 }
 
 resource "random_id" "random" {
-  byte_length = 3
+  byte_length = 4
 }
 
 resource "google_organization_iam_custom_role" "role-svc-check-snapshots" {
@@ -100,10 +100,7 @@ resource "google_organization_iam_custom_role" "role-svc-check-snapshots" {
 resource "google_service_account" "svc-check-snapshots" {
   account_id   = "svc-check-snapshots-${random_id.random.hex}"
   display_name = "Service account for automatically setting up disk snapshots."
-  #project      = module.security.id
   project      = module.snapshots.id
-  #depends_on = [module.security]
-  depends_on = [module.snapshots]
 }
 
 resource "google_project_iam_binding" "project" {
@@ -116,44 +113,33 @@ resource "google_project_iam_binding" "project" {
 
 resource "google_pubsub_topic" "pubsub-snapshots" {
   name = "pubsub-${module.snapshots.name}-${random_id.random.hex}"
-
   project = module.snapshots.id
-
   depends_on = [module.snapshots]
 }
 
 resource "google_project_service" "cloud_scheduler" {
   project = module.snapshots.id
   service = "cloudscheduler.googleapis.com"
-
   timeouts {
     create = "3m"
     update = "6m"
   }
-
   disable_dependent_services = true
-
-  depends_on = [module.snapshots]
 }
 
 resource "google_project_service" "app_engine" {
   project = module.snapshots.id
   service = "appengine.googleapis.com"
-
   timeouts {
     create = "3m"
     update = "6m"
   }
-
   disable_dependent_services = true
-
-  depends_on = [module.snapshots]
 }
 
 resource "google_app_engine_application" "app" {
   project     = module.snapshots.id
   location_id = "us-east1"
-
   depends_on = [
     google_project_service.app_engine
   ]
@@ -165,12 +151,10 @@ resource "google_cloud_scheduler_job" "scheduler-job-snapshots" {
   schedule    = "0 5 * * *"
   region      = "us-east1"
   project     = module.snapshots.id
-
   pubsub_target {
     topic_name = google_pubsub_topic.pubsub-snapshots.id
     data = base64encode("ps-daily-5am")
   }
-
   retry_config {
     retry_count = 0
     max_retry_duration = "0s"
@@ -178,9 +162,7 @@ resource "google_cloud_scheduler_job" "scheduler-job-snapshots" {
     max_backoff_duration = "3600s"
     max_doublings = 5
   }
-
   depends_on = [
-    module.snapshots,
     google_project_service.cloud_scheduler,
     google_project_service.app_engine,
     google_app_engine_application.app
@@ -197,6 +179,14 @@ resource "google_storage_bucket" "backup_records" {
   project = module.snapshots.id
 }
 
+resource "google_storage_bucket_iam_binding" "binding" {
+  bucket = google_storage_bucket.backup_records.name
+  role = google_organization_iam_custom_role.role-svc-check-snapshots.name
+  members = [
+    "serviceAccount:${google_service_account.svc-check-snapshots.email}"
+  ]
+}
+
 resource "google_storage_bucket_object" "archive" {
   name   = "index-${filemd5(var.src_zip)}.zip"
   bucket = google_storage_bucket.bucket.name
@@ -206,63 +196,46 @@ resource "google_storage_bucket_object" "archive" {
 resource "google_project_service" "cloud_functions" {
   project = module.snapshots.id
   service = "cloudfunctions.googleapis.com"
-
   timeouts {
     create = "3m"
     update = "6m"
   }
-
   disable_dependent_services = true
-
-  depends_on = [module.snapshots]
 }
 
 resource "google_project_service" "cloud_build" {
   project = module.snapshots.id
   service = "cloudbuild.googleapis.com"
-
   timeouts {
     create = "3m"
     update = "6m"
   }
-
   disable_dependent_services = true
-
-  depends_on = [module.snapshots]
 }
 
 resource "google_project_service" "cloud_resource_manager" {
   project = module.snapshots.id
   service = "cloudresourcemanager.googleapis.com"
-
   timeouts {
     create = "3m"
     update = "6m"
   }
-
   disable_dependent_services = true
-
-  depends_on = [module.snapshots]
 }
 
 resource "google_project_service" "secretmanager" {
   project = module.snapshots.id
   service = "secretmanager.googleapis.com"
-
   timeouts {
     create = "3m"
     update = "6m"
   }
-
   disable_dependent_services = true
-
-  depends_on = [module.snapshots]
 }
 
 resource "google_secret_manager_secret" "secret-basic" {
   secret_id = "SENDGRID_API_KEY"
   project = module.snapshots.id
-
   replication {
     automatic = true
   }
@@ -271,15 +244,16 @@ resource "google_secret_manager_secret" "secret-basic" {
 
 resource "google_secret_manager_secret_version" "secret-version-basic" {
   secret = google_secret_manager_secret.secret-basic.id
-
   secret_data = var.sg
 }
 
-resource "google_secret_manager_secret_iam_member" "member" {
+resource "google_secret_manager_secret_iam_binding" "binding" {
   project = module.snapshots.id
   secret_id = google_secret_manager_secret.secret-basic.id
   role = google_organization_iam_custom_role.role-svc-check-snapshots.name
-  member = "serviceAccount:${google_service_account.svc-check-snapshots.email}"
+  members = [
+    "serviceAccount:${google_service_account.svc-check-snapshots.email}"
+  ]
 }
 
 resource "google_cloudfunctions_function" "function-snapshots" {
