3c9763f1c0ec78660ae4096be2675f39301b5ca9
Justin <JEStaubach@users.noreply.github.com>
2021-08-01 19:57:20 -0400

    save-progress

--- terraform/vendor/modules/choreographer/main.tfe.workspace.tf
+++ terraform/vendor/modules/choreographer/main.tfe.workspace.tf
@@ -1,7 +1,7 @@
 resource "tfe_workspace" "choreographer" {
   count                 = contains(var.envs, var.environment) ? 1 : 0
 
-  name                  = "${var.organization}-choreographer-${var.environment}"
+  name                  = "${var.tf_org}-choreographer-${var.environment}"
   organization          = var.tf_org
   auto_apply            = true
   file_triggers_enabled = false
