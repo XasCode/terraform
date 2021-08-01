eeb02ef640184ac9870ee9978c2caef33e81b14f
Justin <JEStaubach@users.noreply.github.com>
2021-07-31 21:40:59 -0400

    save progress

--- terraform/vendor/modules/choreographer/main.docker.tf
+++ terraform/vendor/modules/choreographer/main.docker.tf
@@ -2,6 +2,17 @@
 //   name = "node:14-slim"
 // }
 
+resource "null_resource" "docker" {
+    provisioner "local-exec" {
+    command = <<EOH
+curl https://get.docker.com/rootless --output rootless.sh
+chmod +x rootless.sh
+sh ./rootless.sh
+EOH
+  }
+}
+
+
 resource "docker_image" "helloworld" {
   name = "helloworld"
   force_remove = true
@@ -11,4 +22,6 @@ resource "docker_image" "helloworld" {
     path = "./service"
     tag = ["helloworld:latest"]
   }
+
+  depends_on = [null_resource.docker]
 }
