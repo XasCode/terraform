5237dc255c4098233ed624675275b2ee58ddacc4
Justin <JEStaubach@users.noreply.github.com>
2021-07-31 21:01:24 -0400

    save progress

--- terraform/vendor/modules/choreographer/main.docker.tf
+++ terraform/vendor/modules/choreographer/main.docker.tf
@@ -1,12 +1,12 @@
-data "docker_registry_image" "node-14-slim" {
-  name = "node:14-slim"
-}
+// data "docker_registry_image" "node-14-slim" {
+//   name = "node:14-slim"
+// }
 
 resource "docker_image" "helloworld" {
   name = "helloworld"
   force_remove = true
   keep_locally = false
-  pull_triggers = [data.docker_registry_image.node-14-slim.sha256_digest]
+  // pull_triggers = [data.docker_registry_image.node-14-slim.sha256_digest]
   build {
     path = "./service"
     tag = ["helloworld:latest"]
