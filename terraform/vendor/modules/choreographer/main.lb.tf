0e8510ff3573878560997d99687ef41af9b62fe2
Justin <JEStaubach@users.noreply.github.com>
2021-08-03 21:07:07 -0400

    save-progress

--- terraform/vendor/modules/choreographer/main.lb.tf
+++ terraform/vendor/modules/choreographer/main.lb.tf
@@ -8,3 +8,18 @@ resource "google_compute_region_network_endpoint_group" "cloudrun_neg" {
   }
 }
 
+resource "google_compute_backend_service" "default" {
+  provider = google-beta
+  project                         = module.project.id
+  name                            = "backend-service"
+  enable_cdn                      = false
+  timeout_sec                     = 10
+  connection_draining_timeout_sec = 10
+
+  // custom_request_headers          = ["host: ${google_compute_global_network_endpoint.proxy.fqdn}"]
+  // custom_response_headers         = ["X-Cache-Hit: {cdn_cache_status}"]
+
+  backend {
+    group = google_compute_region_network_endpoint_group.cloudrun_neg.id
+  }
+}
