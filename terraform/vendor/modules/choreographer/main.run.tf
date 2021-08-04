08ed23e6c15214c5817ebbd1160809707c8b25d0
Justin <JEStaubach@users.noreply.github.com>
2021-08-03 23:00:20 -0400

    save-progress

--- terraform/vendor/modules/choreographer/main.run.tf
+++ terraform/vendor/modules/choreographer/main.run.tf
@@ -18,3 +18,13 @@ resource "google_cloud_run_service" "default" {
 
     depends_on = [ google_project_service.run[0] ]
 }
+
+resource "google_cloud_run_service_iam_binding" "binding" {
+  location = google_cloud_run_service.default.location
+  project = google_cloud_run_service.default.project
+  service = google_cloud_run_service.default.name
+  role = "roles/viewer"
+  members = [
+    "allUsers",
+  ]
+}
