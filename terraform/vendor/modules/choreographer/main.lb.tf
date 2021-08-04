3b83c194bccb1f8cd9578ed154c94f7fb947907c
Justin <JEStaubach@users.noreply.github.com>
2021-08-03 22:25:23 -0400

    save-progress

--- terraform/vendor/modules/choreographer/main.lb.tf
+++ terraform/vendor/modules/choreographer/main.lb.tf
@@ -66,3 +66,20 @@ resource "google_compute_global_forwarding_rule" "default" {
   port_range = "443"
   ip_address = google_compute_global_address.default.address
 }
+
+resource "google_dns_managed_zone" "xascode" {
+  name        = "xascode"
+  project     = module.project.id
+  dns_name    = "xascode.dev."
+  description = "Example DNS zone"
+}
+
+resource "google_dns_record_set" "api" {
+  provider = "google-beta"
+  project  = module.project.id
+  managed_zone = google_dns_managed_zone.xascode.name
+  name         = "api.xascode.dev."
+  type         = "A"
+  rrdatas      = [google_compute_global_address.default.address]
+  ttl          = 86400
+}
