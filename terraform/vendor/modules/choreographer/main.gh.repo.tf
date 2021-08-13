d929014cad35ea55e6bffeacb7a36bad0bc5cdf3
Justin <JEStaubach@users.noreply.github.com>
2021-08-12 21:06:19 -0400

    save progress

--- terraform/vendor/modules/choreographer/main.gh.repo.tf
+++ terraform/vendor/modules/choreographer/main.gh.repo.tf
@@ -21,3 +21,62 @@ data "github_repository" "repository" {
 
   depends_on = [github_repository.repository]
 }
+
+resource "github_repository_file" "gh_repo_file_keep" {
+  count = length(var.managed)
+
+  repository          = data.github_repository.repository.name
+  branch              = local.branch
+  file                = "src/.keep"
+  content             = <<-EOT
+
+    EOT
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform User"
+  commit_email        = "terraform@xascode.dev"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "gh_repo_file_locals" {
+  count = length(var.managed)
+
+  repository          = data.github_repository.repository.name
+  branch              = local.branch
+  file                = "terraform/locals.tf"
+  content             = <<-EOT
+    locals {
+      project = ${var.managed[count.index].id}
+    }
+    EOT
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform User"
+  commit_email        = "terraform@xascode.dev"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "gh_repo_file_archive" {
+  count = length(var.managed)
+
+  repository          = data.github_repository.repository.name
+  branch              = local.branch
+  file                = "terraform/main.archive.tf"
+  content             = <<-EOT
+    data "archive_file" "srcfiles" {
+      type        = "zip"
+      output_path = "src.zip"
+      source_dir  = "./src"
+    }
+
+    resource "google_storage_bucket_object" "archive" {
+      name   = "src-$${filemd5(data.archive_file.srcfiles.output_path)}.zip"
+      bucket = ${google_storage_bucket.bucket.name}
+      source = data.archive_file.srcfiles.output_path
+    }
+    EOT
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform User"
+  commit_email        = "terraform@xascode.dev"
+  overwrite_on_create = true
+
+  depends_on = [ github_repository_file.gh_repo_file_keep, github_repository_file.gh_repo_file_archive ]
+}
