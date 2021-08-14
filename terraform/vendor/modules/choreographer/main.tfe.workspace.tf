0016644b44b6fe8f03eb10cd5d6386722b9b161c
Justin <JEStaubach@users.noreply.github.com>
2021-08-14 08:03:16 -0400

    save progress

--- terraform/vendor/modules/choreographer/main.tfe.workspace.tf
+++ terraform/vendor/modules/choreographer/main.tfe.workspace.tf
@@ -19,7 +19,7 @@ resource "tfe_workspace" "workspace" {
 resource "tfe_variable" "gc" {
   count                 = contains(var.envs, var.environment) ? length(var.managed) : 0
   key                   = "GOOGLE_CREDENTIALS"
-  value                 = base64decode(google_service_account_key.mykey[count.index].private_key)
+  value                 = trimspace(base64decode(google_service_account_key.mykey[count.index].private_key))
   category              = "env"
   workspace_id          = tfe_workspace.workspace[count.index].id
   sensitive             = true
